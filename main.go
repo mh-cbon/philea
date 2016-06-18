d4c36416efdfde6da7f53bb733e0eae2f454215c
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-18 15:07:32 +0200

    new tokens %f %fname, output format, refactoring

--- main.go
+++ main.go
@@ -38,9 +38,12 @@ Options:
   -d --dry                Show commands only, do not run anything.
 
 Notes:
-  cmd can contain %s, it will be replaced by the current file.
-  cmd can contain %d, it will be replaced by the directory path of the current file.
-  cmd can contain %dname, it will be replaced by the directory name of the current file.
+  cmd can contain
+    %s, it will be replaced by the current file path.
+    %d, it will be replaced by the path of the directory of the current file.
+    %dname, it will be replaced by the directory name of the current file.
+    %f, it will be replaced by the name of the current file.
+    %fname, it will be replaced by the name of the current file minus its extension.
   philea will process all files and all commands and return an exit code=1 if any fails.
 
 Examples:
@@ -80,7 +83,6 @@ Examples:
 	logger.Println("excludeRe=", excludeRe)
 	paths, err := zglob.Glob(wd + "/" + pattern)
 	logger.Println("paths=", paths)
-
 	if len(paths) == 0 {
 		exitWithError(errors.New("No matching files found in this directory"))
 	}
@@ -92,61 +94,87 @@ Examples:
 		exitWithError(errors.New("Pattern has excluded all files!"))
 	}
 
+  cmds = forgeAllCommands(filteredPaths, cmds, wd)
 	errs := make([]error, 0)
-	outs := make([]string, 0)
 	if dry {
-		for _, path := range filteredPaths {
-			for _, cmd := range cmds {
-				fmt.Println(forgeCmd(path, cmd, wd))
-			}
-		}
-	} else {
-		if serie == false {
-			var wg sync.WaitGroup
-			for _, p := range filteredPaths {
-				for _, c := range cmds {
-					wg.Add(1)
-					go func(path string, cmd string) {
-						out, err := executeACommand(path, cmd, wd)
-						outs = append(outs, printOut(cmd, string(out), path, wd))
-						if err != nil {
-							errs = append(errs, err) // racy ?
-						}
-						wg.Done()
-					}(p, c)
-				}
-			}
-			wg.Wait()
-		} else {
-			for _, path := range filteredPaths {
-				for _, cmd := range cmds {
-					out, err := executeACommand(path, cmd, wd)
-					outs = append(outs, printOut(cmd, string(out), path, wd))
-					if err != nil {
-						errs = append(errs, err)
-					}
-				}
-			}
-		}
-	}
+    printCommands(cmds)
+	} else if serie {
+    errs = executeInSeries(cmds, wd, quiet)
+  } else {
+    errs = executeInParallel(cmds, wd, quiet)
+  }
 
 	if quiet == false {
-		for _, out := range outs {
-			fmt.Print(out)
-		}
-		if len(errs) > 0 {
-			fmt.Println("-------------")
-			fmt.Println("There was errors: ", len(errs))
-			for _, err := range errs {
-				fmt.Println(err)
-			}
-		}
+    printErrs(errs)
 	}
 	if len(errs) > 0 {
 		os.Exit(1)
 	}
 }
 
+// printCommands prints forged command on stdout
+// given each cmd and paths
+func printCommands (cmds[]string) {
+  for _, cmd := range cmds {
+    fmt.Println(cmd)
+  }
+}
+
+// executeInParallel execute each cmds for each given path
+// prints command output to stdout asap, as one block
+// blocks until all commands are done
+func executeInParallel (cmds []string, wd string, quiet bool) []error {
+	errs := make([]error, 0)
+  var wg sync.WaitGroup
+  for _, c := range cmds {
+    wg.Add(1)
+    go func(cmd string) {
+      out, err := executeACommand(cmd, wd)
+      if err==nil && quiet == false {
+        fOut := printOut(cmd, string(out))
+        fmt.Print(fOut)
+      }
+      if err != nil {
+        errs = append(errs, err) // racy ?
+      }
+      wg.Done()
+    }(c)
+  }
+  wg.Wait()
+  return errs
+}
+
+// executeInSeries execute each cmds for each given path
+// prints command output to stdout asap
+func executeInSeries (cmds []string, wd string, quiet bool) []error {
+	errs := make([]error, 0)
+  for _, cmd := range cmds {
+    out, err := executeACommand(cmd, wd)
+    if err==nil && quiet == false {
+      fOut := printOut(cmd, string(out))
+      fmt.Print(fOut)
+    }
+    if err != nil {
+      errs = append(errs, err)
+    }
+  }
+  return errs
+}
+
+// printErrs print errors on stdout
+func printErrs (errs []error) {
+  if len(errs) > 0 {
+    fmt.Println("")
+    fmt.Println("-------------")
+    fmt.Printf("There were %d error(s)\n", len(errs))
+    for _, err := range errs {
+      fmt.Println(err)
+    }
+  }
+}
+
+// exitWithError exits program is an error is provided
+// prints the error on stdout
 func exitWithError(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -154,9 +182,11 @@ func exitWithError(err error) {
 	}
 }
 
-func printOut(cmd string, out string, path string, wd string) string {
+// printOut prints a command output on stdout by splitting each line
+// and preprending them with the corresponding command line
+func printOut(cmd string, out string) string {
 	ret := ""
-	cmd = forgeCmd(path, cmd, wd)
+  out = strings.TrimSpace(out)
 	for _, line := range strings.Split(out, "\n") {
 		ret = ret + cmd + ": " + line + "\n"
 	}
@@ -166,8 +196,10 @@ func printOut(cmd string, out string, path string, wd string) string {
 	return ret
 }
 
-func executeACommand(path string, cmd string, wd string) ([]byte, error) {
-	cmd = forgeCmd(path, cmd, wd)
+// executeACommand runs a command string
+// using wd as the command working directory
+// command Env is equal to os.Environ()
+func executeACommand(cmd string, wd string) ([]byte, error) {
 	parts := strings.Split(cmd, " ")
 	if len(parts) == 0 {
 		return make([]byte, 0), errors.New("Failed to parse cmd: " + cmd)
@@ -182,6 +214,9 @@ func executeACommand(path string, cmd string, wd string) ([]byte, error) {
 	return oCmd.CombinedOutput()
 }
 
+// forgeCmd takes a subject path and a command string cmd
+// replaces occurrences of %dname %d and %s appropriately
+// and returns the newly forged command string
 func forgeCmd(path string, cmd string, wd string) string {
 	logger.Printf("forgeCmd in  cmd='%s' path='%s'", cmd, path)
 	dir := filepath.Dir(path)
@@ -192,7 +227,11 @@ func forgeCmd(path string, cmd string, wd string) string {
 	if dir == "" {
 		dir = "."
 	}
+  f := filepath.Base(path)
+	fname := strings.Replace(f, filepath.Ext(f), "", -1)
 	dname := filepath.Base(dir)
+	cmd = strings.Replace(cmd, "%fname", fname, -1)
+	cmd = strings.Replace(cmd, "%f", f, -1)
 	cmd = strings.Replace(cmd, "%dname", dname, -1)
 	cmd = strings.Replace(cmd, "%s", path, -1)
 	cmd = strings.Replace(cmd, "%d", dir, -1)
@@ -200,6 +239,20 @@ func forgeCmd(path string, cmd string, wd string) string {
 	return cmd
 }
 
+// forgeAllCommands forges all commands strings
+// given each path and cmd
+func forgeAllCommands (paths []string, cmds[]string, wd string) []string {
+	ret := make([]string, 0)
+  for _, path := range paths {
+    for _, cmd := range cmds {
+      ret = append(ret, forgeCmd(path, cmd, wd))
+    }
+  }
+  return ret
+}
+
+// filterPaths takes a list of path in input
+// and removes all values matching exclude
 func filterPaths(paths []string, exclude *regexp.Regexp) []string {
 	filtered := make([]string, 0)
 	for _, v := range paths {
@@ -210,6 +263,9 @@ func filterPaths(paths []string, exclude *regexp.Regexp) []string {
 	return filtered
 }
 
+// getExlcudeRe creates a regexp object from a string
+// ** are replaced by .+
+// * are replaced by .+ (@todo change to [^/\\]+ ?)
 func getExlcudeRe(exclude string, sensitive bool) (*regexp.Regexp, error) {
 	exclude = strings.Replace(exclude, "**", ".+", -1)
 	exclude = strings.Replace(exclude, "*", ".+", -1)
@@ -220,6 +276,7 @@ func getExlcudeRe(exclude string, sensitive bool) (*regexp.Regexp, error) {
 	return regexp.Compile(flags + exclude)
 }
 
+// Helper to get commands string passed on command line
 func getCmds(arguments map[string]interface{}) []string {
 	cmds := make([]string, 0)
 	if c, ok := arguments["<cmds>"].([]string); ok {
@@ -228,6 +285,7 @@ func getCmds(arguments map[string]interface{}) []string {
 	return cmds
 }
 
+// helper to get exclude argument passed on the command line
 func getExclude(arguments map[string]interface{}) string {
 	exclude := "*vendor/*"
 	if val, ok := arguments["--exclude"].(string); ok {
@@ -240,6 +298,7 @@ func getExclude(arguments map[string]interface{}) string {
 	return exclude
 }
 
+// helper to get wd argument passed on the command line
 func getWd(arguments map[string]interface{}) string {
 	dir := ""
 	if val, ok := arguments["--change-dir"].(string); ok {
@@ -252,6 +311,7 @@ func getWd(arguments map[string]interface{}) string {
 	return dir
 }
 
+// helper to get pttern argument passed on the command line
 func getPattern(arguments map[string]interface{}) string {
 	pattern := "**/*.go"
 	if val, ok := arguments["--pattern"].(string); ok {
@@ -264,6 +324,7 @@ func getPattern(arguments map[string]interface{}) string {
 	return pattern
 }
 
+// helper to get quiet argument passed on the command line
 func isQuiet(arguments map[string]interface{}) bool {
 	quiet := false
 	if isQuiet, ok := arguments["--quiet"].(bool); ok {
@@ -276,6 +337,7 @@ func isQuiet(arguments map[string]interface{}) bool {
 	return quiet
 }
 
+// helper to get dry argument passed on the command line
 func isDry(arguments map[string]interface{}) bool {
 	dry := false
 	if isDry, ok := arguments["--dry"].(bool); ok {
@@ -288,6 +350,7 @@ func isDry(arguments map[string]interface{}) bool {
 	return dry
 }
 
+// helper to get series argument passed on the command line
 func isSerie(arguments map[string]interface{}) bool {
 	serie := false
 	if inSerie, ok := arguments["--series"].(bool); ok {
